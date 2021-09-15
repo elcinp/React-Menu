4a6f1f62847ae45fe89c3f401f05564076d497a0
elcinp <elcinpars1@gmail.com>
2021-09-15 22:46:59 +0300

    categories.map

--- src/Categories.js
+++ src/Categories.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Categories = () => {
   return(
-    
+    <div className="btn-container">
+      {Categories.map((categories,index) => {
+        return(
+          <button>
+            {category}
+          </button>
+        )
+      })}
+    </div>
   )
 };
 
